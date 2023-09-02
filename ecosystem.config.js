bc096b6cde16136433a85f50df0e617c71bb69f3
Gyeongho Park <kayheypark@gmail.com>
2023-09-03 04:52:31 +0900

    feat: ecosystem.config.js - 배포환경별 앱 구분(배열), package.json - npm 스크립트 중 pm2 에서 --only 옵션 사용

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,24 +1,40 @@
 module.exports = {
   apps: [
     {
-      name: 'my-app',
+      name: 'web-prod',
       exec_mode: 'cluster',
       instances: 'max',
+      script: 'node_modules/next/dist/bin/next',
+      watch: false,
+      args: 'start',
+      env_production: {
+        APP_ENV: 'prod',
+        PORT: '3003'
+      }
+    },
+    {
+      name: 'web-stag',
+      exec_mode: 'cluster',
+      instances: '2',
+      script: 'node_modules/next/dist/bin/next',
+      watch: false,
+      args: 'start',
+      env_staging: {
+        APP_ENV: 'stag',
+        PORT: '3002'
+      }
+    },
+    {
+      name: 'web-dev',
+      exec_mode: 'cluster',
+      instances: '2',
       script: 'node_modules/next/dist/bin/next',
       watch: false,
       args: 'start',
       env_development: {
         APP_ENV: 'dev',
         PORT: '3001'
-      },
-      env_staging: {
-        APP_ENV: 'stag',
-        PORT: '3002'
-      },
-      env_production: {
-        APP_ENV: 'prod',
-        PORT: '3003'
       }
-    }
+    },
   ]
 }
